edd84a5713f215c0296d237e0fb5939aa596dbd4
Ashabenny123 <110253397+Ashabenny123@users.noreply.github.com>
2023-02-06 11:56:08 +0530

    add custom config

---
+++ tools/build/app-env.ts
@@ -0,0 +1,38 @@
+declare global {
+  /**
+   * @description
+   * Global commandline environment variables available to
+   * devserver build at runtime
+   *
+   */
+  export const APP_ENV: {
+    /**
+     * @description
+     * Flag toggle ngrx runtime checks
+     *
+     * @usage
+     * `$ yarn serve --no-runtimeChecks`
+     *
+     */
+    runtimeChecks: boolean;
+
+    /**
+     * @description
+     * Log exceptions to third party exception tracker (e.g. Sentry, Log Rocket)
+     */
+    logErrors: boolean;
+
+    /**
+     * @description
+     * Url to post exception logs
+     *
+     */
+    loggerUrl: string;
+  };
+}
+
+export const DEFAULT_APP_ENV: typeof APP_ENV = {
+  runtimeChecks: true,
+  logErrors: false,
+  loggerUrl: 'http://localhost:3000',
+};
---
+++ tools/build/serve.ts
@@ -0,0 +1,20 @@
+import * as yargs from 'yargs';
+import { spawn } from 'child_process';
+import { DEFAULT_APP_ENV } from './app-env';
+
+const parseNgArgs = () =>
+  process.argv.filter(
+    (arg, index) =>
+      index > 1 &&
+      Object.keys(DEFAULT_APP_ENV).every((key) => !arg.includes(key))
+  );
+
+const ngArgs = parseNgArgs();
+
+const argv = { ...DEFAULT_APP_ENV, ...yargs.argv };
+
+const childProcess = spawn('npm', ['start', ...ngArgs], {
+  stdio: 'inherit',
+  env: { ...process.env, APP_ENV: JSON.stringify(argv) },
+  shell: true,
+});
